d14f3ff9a51a7f95bf810f2496bbf84f6f664990
Daniel Howes <dan.howes29@outlook.com>
2025-12-10 09:38:06 +0000

    Create table in DB if it doesn't exist already

--- orgs_database_builder_v.1.py
+++ orgs_database_builder_v.1.py
@@ -2,9 +2,8 @@
 import pandas as pd
 import os
 import uuid
-from sqlalchemy import NVARCHAR
-from sqlalchemy.dialects.mssql import BIT, DATE
-from sqlalchemy import text
+from sqlalchemy import NVARCHAR, Uuid
+from sqlalchemy.dialects.mssql import DATE
 import ds_utils.database_operations as dbo
 
 # %%
@@ -29,16 +28,15 @@ dropped_cols = [
 df_edited = df_edited.drop(columns=dropped_cols)
 
 # %%
+# Add a UUID column
+df_edited.insert(0, 'uuid', [uuid.uuid4() for _ in range(len(df_edited))])
+
+# %%
 # Creat start and end date columns
 df_edited[['start_date', 'end_date']] = [None, None]
 
-# %% 
-# Add UUID column
-
-df_edited.insert(0, 'uuid', [uuid.uuid4() for _ in range(len(df))])
-
 # %%
-# Connect to database
+# Create connection to database
 engine = dbo.connect_sql_db(
     driver="pyodbc",
     driver_version=os.environ["ODBC_DRIVER"],
@@ -49,3 +47,29 @@ engine = dbo.connect_sql_db(
     username=os.environ["AZURE_CLIENT_ID"],
     password=os.environ["AZURE_CLIENT_SECRET"],
 )
+
+# %%
+# Create table in DB if it doesn't exist, otherwise skip
+try:
+    df_edited.to_sql(
+        name='govuk_orgs',
+        schema='testing',
+        con=engine,
+        if_exists='fail',
+        index=False,
+        dtype={
+            'uuid': Uuid,
+            'id': NVARCHAR(200),
+            'title': NVARCHAR(200),
+            'format': NVARCHAR(100),
+            'web_url': NVARCHAR(200),
+            'analytics_identifier': NVARCHAR(100),
+            'closed_at': DATE,
+            'govuk_status': NVARCHAR(100),
+            'govuk_closed_status': NVARCHAR(100),
+            'start_date': DATE,
+            'end_date': DATE
+        }
+    )
+except ValueError:
+    pass
